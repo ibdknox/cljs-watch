5e03f54340c77421584d5b4686cbd1661d8a3b97
Sam Ritchie <sritchie09@gmail.com>
2011-11-10 16:52:27 -0500

    Added ability to compile clj files with {:generic true} added to the namespace metadata.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cljs-watch "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "The Clojurescript compilation watcher."
   :main cljs-watch.core
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [cljs-compiler-jar "0.1.0-SNAPSHOT"]])
