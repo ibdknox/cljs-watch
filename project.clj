f2b6b2b9d04155451c459c2d5979c6c10b479499
Benjamin Peter <benjaminpeter@arcor.de>
2012-02-24 22:51:09 +0100

    Used CLASSPATH evn variable to set the path instead of -cp. Added command line parsing library to cljs-watch. Added bell feature. Added custom bell command feature.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject cljs-watch "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "cljs-watch automatically recompile your clourescript code"
   :main cljs-watch.core
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.cli "0.2.1"] 
                  [cljs-compiler-jar "0.1.0-SNAPSHOT"]])
